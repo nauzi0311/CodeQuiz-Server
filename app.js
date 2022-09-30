1f4ac5fbe2e4844fafe617f729c46e6bc949460f
nauzi <tanimototakaaki.t@gmail.com>
2022-09-30 10:34:45 +0900

    2022/09/30

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
 var memoRouter = require('./routes/memo');
 var questionRouter = require('./routes/question');
 var calenderRouter = require('./routes/calender');
 var resultRouter = require('./routes/result');
 var detailRouter = require('./routes/detail');
-var addRouter = require('./routes/add');
-var levelRouter = require('./routes/level');
+//var addRouter = require('./routes/add');
+
 
 var app = express();
 
@@ -30,22 +29,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/index', indexRouter);
-//app.use('/user', userRouter);
 app.use('/memo', memoRouter);
-app.use('/quest',questionRouter);
-app.use('/calender',calenderRouter);
-app.use('/result',resultRouter);
-app.use('/detail',detailRouter);
+app.use('/quest', questionRouter);
+app.use('/calender', calenderRouter);
+app.use('/result', resultRouter);
+app.use('/detail', detailRouter);
 // app.use('/add',addRouter);
-// app.use('/level',levelRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/result.js
+++ routes/result.js
@@ -13,7 +13,7 @@ const { log } = require('console');
   when get the json like above, send each json file.
 */
 router.post('/', function (req, res, next) {
-  const [device, level, point, id_list, correct_list, second_list, user_answer] = ResultData(req.body);
+  const [device, level, point, id_list, correct_list, second_list, user_answer, badge] = ResultData(req.body);
   var user_data = GetUserData(device);
   //change level
   user_data.level = level;
@@ -49,39 +49,67 @@ router.post('/', function (req, res, next) {
 
   //badge process
   //need
+  {
+    switch (badge) {
+      case 0:
+        break;
+      case 1://最初の７問ききる
+      case 2://レベル３になる
+      case 3://レベル５になる
+      case 4://レベル７になる
+      case 5://レベル９になる
+      case 6://レベル10になる
+      case 7://レベル13になる
+      case 8://レベル15になる
+        user_data.badge[badge] = true;
+        break;
+      case 9://3日連続ログイン
+      case 10://5日連続ログイン
+      case 11://7日連続ログイン
+      case 12://10日連続ログイン
+      case 13://14日連続ログイン
+      case 14://17日連続ログイン
+      case 15://21日連続ログイン
+      case 16://3日ログイン
+      case 17://5日ログイン
+      case 18://7日ログイン
+      case 19://10日ログイン
+      case 20://
+    }
+  }
 
   //write ranking file
   {
     const user_file = ReadJSONFile('./data/user.json');
     var username;
-    for(i = 0; i < user_file.length; i++) {
-      if(user_file[i].device == device) {
+    for (i = 0; i < user_file.length; i++) {
+      if (user_file[i].device == device) {
         username = user_file[i].username;
       }
     }
-    
+
     var ranking_file = ReadJSONFile('./data/ranking.json');
-    for(i = 0; i < ranking_file.length; i++){
-      if(ranking_file[i].username == username){
+    for (i = 0; i < ranking_file.length; i++) {
+      if (ranking_file[i].username == username) {
         ranking_file[i].level = level;
         ranking_file[i].point = point;
       }
     }
-    
+
     //sort
     //need fix
-    if(ranking_file.length >= 2){
-      ranking_file.sort(function(first,second){
-        if(first.level < second.level){
+    if (ranking_file.length >= 2) {
+      ranking_file.sort(function (first, second) {
+        if (first.level < second.level) {
           return -1;
-        }else if(first.level > second.level){
+        } else if (first.level > second.level) {
           return 1;
-        }else{
+        } else {
           return 0;
         }
       })
     }
-    WriteNewJSONFile('./data/ranking.json',ranking_file);
+    WriteNewJSONFile('./data/ranking.json', ranking_file);
   }
 
   WriteNewJSONFile('./data/user/' + device + '.json', user_data);
@@ -108,7 +136,7 @@ router.post('/', function (req, res, next) {
 
 
 function ResultData(body) {
-  return [body.device, body.level, body.point, body.id_list, body.correct_list, body.second_list, body.user_answer];
+  return [body.device, body.level, body.point, body.id_list, body.correct_list, body.second_list, body.user_answer, body.badge];
 }
 
 function GetCourseNameFromId(ids) {
