de4a75e5a69246509472b56b2bb5ea16d425d499
nauzi <tanimototakaaki.t@gmail.com>
2022-11-04 14:32:12 +0900

    add question

--- test.c
+++ test.c
@@ -1,9 +1,19 @@
+#define DEBUG 1
+#define ADD(a,b) a+b
 #include <stdio.h>
 
+#if DEBUG == 0
 int main(int argc,char* argv[]){
-  char s[8];
-  fprintf(stdin,"%s","aquatan");
-  fscanf(stdin,"%s",s);
-  printf("%s",s);
+  int x = 3,y = 4;
+  printf("%d",2*ADD(x,y));
   return 0;
 }
+#endif
+
+#if DEBUG == 1
+int main(int argc,char* argv[]){
+  int x = 3,y = 4;
+  printf("%d%d%d",x,y,ADD(x,y));
+  return 0;
+}
+#endif
