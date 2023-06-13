ef6aaa3b2cdf6b1d05a458c5ccad347d999569c2
nauzi <tanimototakaaki.t@gmail.com>
2023-06-13 11:34:58 +0900

    FIX:question data

--- test.c
+++ test.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define SIZE(list) (sizeof(list)/sizeof(list[0]))
 
-typedef struct data{
-  int number;
-  float point;
-}DATA;
-
-int main()
+int main(int argc, char const *argv[])
 {
-  DATA d = {2,20.0};
-  DATA *p = &d;
-  printf("%f",p->point);
+  int i,j,k,l[] = {2,0,2,3};
+  for(i = 1;i < SIZE(l);i++){
+    if(l[i-1] > l[i]){
+      for(j = 0;j < i;j++){
+        if(l[j] > l[i]){
+          int target = l[i];
+          for(k = i;j < k;k--){
+            l[k] = l[k-1];
+          }
+          l[j] = target;
+        }
+      }
+    }
+  }
+
+  for (i = 0; i < SIZE(l); i++){
+    printf("%d",l[i]);
+  }
+  
   return 0;
 }
