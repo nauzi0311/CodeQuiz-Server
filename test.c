3b05eb9d364be7f9ea2fd510bc5441fe327881d0
nauzi <tanimototakaaki.t@gmail.com>
2023-06-14 09:17:25 +0900

    FIX:question data

--- test.c
+++ test.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct data{
-  int id;
-  char name[50];
-}DATA;
+int add(int a,int b){
+  return a + b;
+}
 
-void Print(DATA _d){
-  printf("id: %d\
-  name: %s", 
-  _d.id,_d.name);
+int sub(int a,int b){
+  return a - b;
+}
+
+int ???(int,int){
+  return i == 0 ? add : sub;
 }
 
 int main()
 {
-  DATA a = { 2000,"Job" };
-  Print(a);
+  printf("%d",select(0)(5,3));
   return 0;
 }
