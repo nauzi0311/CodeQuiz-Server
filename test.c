b9cf09e311284f24b7602e4e92d4e65a181a7ff5
nauzi0311 <tanimototakaaki.t@gmail.com>
2023-09-05 15:51:44 +0900

    ADD:soft1 data

--- test.c
+++ test.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main(){
-  int x,y,z;
-  x = 2;y = 6;z = 36;
-  z = z / y / x;
-  printf("%d",z);
-  return (0);
+  char list[][5] = {
+    {'a','q','\0','b','x'},
+    {'u','a','t','\0','y'},
+    {'a','n','\0','w','z'},
+  };
+  int i;
+  for(i = 0;i < 3;i++){
+    printf("%s",list[i]);
+  }
+  return 0;
 }
