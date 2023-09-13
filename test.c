5e8e9fdf97641c7ae0352b6132db47ce3e9fe35b
nauzi0311 <tanimototakaaki.t@gmail.com>
2023-09-13 15:46:11 +0900

    FIX:soft1 data stage 1

--- test.c
+++ test.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
-
-typedef struct test{
-  int x;
-  char c;
-}TEST;
-
-int main(int argc,char* argv[]){
-  TEST list[5] = {{1,'a'},{2,'b'},{3,'c'},{4,'d'},{5,'e'}};
-  TEST *p;
-  p = list;
-  printf("%d",???);
-  return 0;
+int main(){
+  printf("Hello, World!");
+  return (0);
 }
