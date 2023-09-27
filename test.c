b8b47101024b639210094b3c2442ff0f54af36ec
nauzi0311 <tanimototakaaki.t@gmail.com>
2023-09-27 14:12:41 +0900

    FIX:question data 1411.json

--- test.c
+++ test.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main(){
-	long long ll = 2e9;
-	long long *pll = &ll;
-	printf("%d",???);
-	return 0;
+	int i,j;
+	for(i = 0;i < 3;i++){
+		for(j = 0;j < 1;j++){
+		if(j == 1)???;
+		printf("%d",2*i+j);
+		}
+	} return 0;
 }
