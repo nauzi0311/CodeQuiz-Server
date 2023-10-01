296eefbd7db40f81ee8f1c9f2ed592b3a2aaa526
nauzi0311 <tanimototakaaki.t@gmail.com>
2023-10-01 22:49:55 +0900

    FIX:question data

--- test.c
+++ test.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(){
-  int list[] = {0,1,2};
-  printf("%p\n",&list[1]);
-  printf("%p\n",&list[2]);
-  printf("%p\n",&list[3]);
+  int list[] = {10,20,30,40};
+  int size = sizeof(NULL);
+  printf("%d",size);
   return 0;
 }
