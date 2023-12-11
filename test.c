0697df5ad9ac84139c5f215e560f46d595af97c4
nauzi0311 <tanimototakaaki.t@gmail.com>
2023-12-11 21:44:29 +0900

    FIX:question soft1/15

--- test.c
+++ test.c
@@ -1,12 +1,15 @@
+#define STRCAT(a,b) a##b
 #include <stdio.h>
 
-void func(int a,???);
-
-int main(){
-  func(10,20);
-  return 0;
+int f1(int a,int b){
+  return a + b;
 }
 
-void func(int a,int b){
-  printf("%d",a+b);
+int f2(int a,int b){
+  return a - b;
+}
+
+int main(int argc,char* argv[]){
+  printf("%d",STRCAT(???)(20,10));
+  return 0;
 }
